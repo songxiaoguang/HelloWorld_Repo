0cfa233227a7ec4c8428d3eeee038436d5c0c4c8
Song Xiaoguang <xgsong@mobvoi.com>
2016-07-07 15:11:31 +0800

    add compare funciton

--- src/HelloWorld.c
+++ src/HelloWorld.c
@@ -4,10 +4,18 @@ void add(int,int);
 
 void minus(int,int);
 
+void compare(int a,int b){
+	if (a > b)
+		printf("%d > %d\n",a,b);
+	else
+		printf("%d < %d\n",a,b);
+}
+
 int main(){
 	printf("Hello World\n");
 	add(3,5);
 	minus(10,4);
+	compare(15,9);
 	return 0;
 }
 
