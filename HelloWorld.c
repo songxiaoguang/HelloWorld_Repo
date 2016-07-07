13d91a76cdc3f96158446c31f07c574ae5a57866
Song Xiaoguang <xgsong@mobvoi.com>
2016-07-07 10:19:39 +0800

    Add add function

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 
+void add(int,int);
+
 int main(){
-	printf("Hello World");
+	printf("Hello World\n");
+	add(3,5);
 	return 1;
+}
+
+void add(int a,int b){
+	printf("%d + %d = %d\n",a,b, a + b);
 }
