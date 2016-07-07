50a6e33ef3f0e50f3651304d113e0070f475ceb0
Song Xiaoguang <xgsong@mobvoi.com>
2016-07-07 10:40:36 +0800

    Add minus function

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,12 +2,19 @@
 
 void add(int,int);
 
+void minus(int,int);
+
 int main(){
 	printf("Hello World\n");
 	add(3,5);
-	return 1;
+	minus(10,4);
+	return 0;
 }
 
 void add(int a,int b){
 	printf("%d + %d = %d\n",a,b, a + b);
+}
+
+void minus(int a,int b){
+	printf("%d - %d = %d\n",a,b,a - b);
 }
